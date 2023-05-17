c6720c956f8bd0f4bb011293742472ff2954a96d
Quentin-Marecat <quentin.marecat@etu.umontpellier.fr>
2023-05-17 10:25:47 +0200

    'Q : herku deploy'

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
-$target_dir = "";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-$filename = basename($_FILES["fileToUpload"]["name"]);
-echo '<a href="' . $url_pdf . $filename'">Cliquez ici pour télécharger le PDF</a>';
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['pdf_file'])) {
+    $file = $_FILES['pdf_file'];
+    
+    // Vérifier si le fichier a été téléchargé sans erreur
+    if ($file['error'] === UPLOAD_ERR_OK) {
+        // Définir le chemin de destination du fichier
+        $destination = './' . $file['name'];
+        
+        // Déplacer le fichier vers le dossier de destination
+        move_uploaded_file($file['tmp_name'], $destination);
+        
+        // Récupérer l'URL du fichier
+        $url_pdf = 'https://pdf-drag-drop.herokuapp.com/' . $file['name'];
+        
+        // Afficher l'URL du fichier
+        echo 'Lien vers le PDF : <a href="' . $url_pdf . '">' . $url_pdf . '</a>';
+    } else {
+        echo 'Une erreur s\'est produite lors du téléchargement du fichier.';
+    }
+}
 ?>
-
