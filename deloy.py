2a8409018f3918c741e0380ea50d402556d4844d
Quentin-Marecat <quentin.marecat@etu.umontpellier.fr>
2023-05-17 09:49:43 +0200

    Q : heroku setup

--- deloy.py
+++ deloy.py
@@ -6,8 +6,9 @@ import subprocess
 
 def deploy_to_heroku():
     app_name = "pdf-drag-drop"  # Change this to your desired app name
-#    subprocess.run(["heroku", "create", app_name])
-    subprocess.run(["git", "push", f"heroku master"])
+    subprocess.run(["heroku", "git:remote", "-a", app_name])
+    subprocess.run(["git", "push", f"heroku main"])
 
 if __name__ == "__main__":
     deploy_to_heroku()
+
